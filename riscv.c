cfb2d069ae10bab7393175bebaf0d96c3e777ee0
SeaOfMares <maresdavide6704@outlook.com>
2022-03-22 01:10:30 -0500

    Last commit

--- riscv.c
+++ riscv.c
@@ -67,22 +67,45 @@ int power(int base, int exp){
 
 //parses a string into an integer
 int integerParser(char * str){
+	printf("%s\n",str);
 	int count = 0;
 	int i = 0;
 	int negative = 1;
 	if(str[0] == '-'){
 		negative*=-1;
 		++i;
-	}
+	}else if(str[0] == '+') ++i;
 	int size = arraySize(str);
+	//printf("%d\n",size);
 	int place;
-	while(str[i] != '\0'){
+	while(str[i] != '\0' && str[i] != ' '){
+		if(str[i] - '0' > 9) return -1;
 		place = power(10,(size-i));
 		count+= (str[i] - '0')* place;
+		//printf("%c %d\n",str[i],count);
 		++i;
 	}
 	count/=10;
 	return count*negative;
+}
+
+bool checkInst(int rd, int rs1, int rs2, int imm){
+	//check if rd,rs1, and rs2 are not integers or not found in the register
+	//for example rs1 should not equal 100 or -1 since they are not in the register
+	if(rd == -1 || rd < 0 || rd > 31){
+		printf("rd is incorrect | rd = %d\n",rd);
+		return false;
+	}else if(rs1 == -1 || rs1 < 0 || rs1 > 31){
+		printf("rs1 is incorrect | rs1 = %d\n",rs1);
+		return false;
+	}else if(rs2 == -1 || rs2 < 0 || rs2 > 31){
+		printf("rs2 is incorrect | rs2 = %d\n",rs2);
+		return false;
+	}else if(imm == -1){
+		printf("imm is incorrect | imm = %d\n",imm);
+		return false;
+	}
+	return true;
 }
 /**
  * Fill out this function and use it to read interpret user input to execute RV64 instructions.
@@ -91,7 +114,6 @@ int integerParser(char * str){
  */
 bool interpret(char* instr){
 	char ** tokens = tokenize(instr,' ');
-	printf("%s\n",tokens[0]);
 	int rd;
 	int rs1;
 	int rs2;
@@ -104,11 +126,12 @@ bool interpret(char* instr){
 		//tokenizes 1000(x5) -> 1000 x5)
 		char ** source = tokenize(tokens[2], '(');
 		rd = integerParser(split_str(tokens[1],1,arraySize(tokens[1])));
-		rs1 = integerParser(split_str(source[1],1,arraySize(source[1])-1));
+		rs1 = integerParser(split_str(source[1],1,arraySize(source[1])-2));
 		imm = integerParser(source[0]);
+		if(!checkInst(rd,rs1,0,imm)) return false;
 		int32_t address = reg[rs1] + imm;
 		reg[rd] = read_address(address, mem_file);
-		return true;
+		free(source);
 	}
 	//SW x5 40(x6)
 	//from register to memory -> Memory[x6 + 40] = x5
@@ -117,59 +140,66 @@ bool interpret(char* instr){
 		//tokenizes 40(x6) -> 40 x6)
 		char ** source = tokenize(tokens[2], '(');
 		rd = integerParser(split_str(tokens[1],1,arraySize(tokens[1])));
-		rs1 = integerParser(split_str(source[1],1,arraySize(source[1])-1));
+		rs1 = integerParser(split_str(source[1],1,arraySize(source[1])-2));
 		imm = integerParser(source[0]);
+		if(!checkInst(rd,rs1,0,imm)) return false;
 		int32_t address = reg[rs1] + imm;
 		int32_t data_to_write = reg[rd];
 		int32_t write = write_address(data_to_write, address, mem_file);
-		return true;
+		free(source);
+		if(write == (int32_t) NULL)
+		printf("ERROR: Unsucessful write to address %0X\n", address);
 	}
 	//ADD X6 X0 X29
 	else if(checkString(tokens[0], "ADD") == 0)
 	{
-		rd = integerParser(tokens[1]);
-		rs1 = integerParser(tokens[2]);
-		rs2 = integerParser(tokens[3]);
+		rd = integerParser(split_str(tokens[1],1,arraySize(tokens[1])));
+		rs1 = integerParser(split_str(tokens[2],1,arraySize(tokens[2])));
+		rs2 = integerParser(split_str(tokens[3],1,arraySize(tokens[3])-1));
+		//printf("%d %d %d\n",rd,rs1,rs2);
+		if(!checkInst(rd,rs1,rs2,0)) return false;
 		reg[rd] = reg[rs1] + reg[rs2];
-		return true;
 	}
-	//ADD X6 X0 329
+	//ADDI X6 X0 329
 	else if(checkString(tokens[0], "ADDI") == 0)
 	{
-		rd = integerParser(tokens[1]);
-		rs1 = integerParser(tokens[2]);
-		imm = integerParser(tokens[3]);
+		rd = integerParser(split_str(tokens[1],1,arraySize(tokens[1])));
+		rs1 = integerParser(split_str(tokens[2],1,arraySize(tokens[2])));
+		imm = integerParser(split_str(tokens[3],0,arraySize(tokens[3])-1));
+		if(!checkInst(rd,rs1,0,imm)) return false;
 		reg[rd] = reg[rs1] + imm;
-		return true;
 	}
 	//AND X6 X0 X29
 	else if(checkString(tokens[0], "AND") == 0)
 	{
-		rd = integerParser(tokens[1]);
-		rs1 = integerParser(tokens[2]);
-		imm = integerParser(tokens[3]);
+		rd = integerParser(split_str(tokens[1],1,arraySize(tokens[1])));
+		rs1 = integerParser(split_str(tokens[2],1,arraySize(tokens[2])));
+		rs2 = integerParser(split_str(tokens[3],1,arraySize(tokens[3])-1));
+		if(!checkInst(rd,rs1,rs2,0)) return false;
 		reg[rd] = reg[rs1] & reg[rs2];
-		return true;
 	}
 	//OR X6 X0 X29
 	else if(checkString(tokens[0], "OR") == 0)
 	{
-		rd = integerParser(tokens[1]);
-		rs1 = integerParser(tokens[2]);
-		imm = integerParser(tokens[3]);
+		rd = integerParser(split_str(tokens[1],1,arraySize(tokens[1])));
+		rs1 = integerParser(split_str(tokens[2],1,arraySize(tokens[2])));
+		rs2 = integerParser(split_str(tokens[3],1,arraySize(tokens[3])-1));
+		if(!checkInst(rd,rs1,rs2,0)) return false;
 		reg[rd] = reg[rs1] | reg[rs2];
-		return true;
 	}
 	//XOR X6 X0 X29
 	else if(checkString(tokens[0], "XOR") == 0)
 	{
-		rd = integerParser(tokens[1]);
-		rs1 = integerParser(tokens[2]);
-		imm = integerParser(tokens[3]);
+		rd = integerParser(split_str(tokens[1],1,arraySize(tokens[1])));
+		rs1 = integerParser(split_str(tokens[2],1,arraySize(tokens[2])));
+		rs2 = integerParser(split_str(tokens[3],1,arraySize(tokens[3])-1));
+		if(!checkInst(rd,rs1,rs2,0)) return false;
 		reg[rd] = reg[rs1] ^ reg[rs2];
-		return true;
+	}else{
+		printf("Couldn't identify operation\n");
+		return false;
 	}
-	return false;
+	return true;
 }
 
 
@@ -210,18 +240,23 @@ int main(){
 	// Below is a sample program to a write-read. Overwrite this with your own code.
 	//write_read_demo();
 	
-	//char str[20] = {'A','D','D',' ','X','\0'};
-	printRegistry();
+	//printRegistry();
 	printf("Please input instruction: ");
-	char * str;
-	str = "LW x7 1000(x5)";
-	//fgets(str,20,stdin);
-	printf("%s\n",str);
-	interpret(str);
-	printRegistry();
-	int32_t address = 1000 + reg[5];
-	int32_t read = read_address(address, "mem.txt");
+	char * str[20];
+	//str = "LW x7 1000(x5)";
+	//interpret(str);
+	
+	char * p = fgets(str,20,stdin);
 
-	printf("Read address %lu (0x%lX): %lu (0x%lX)\n", address, address, read, read);
+	printf("\n");
+	while(p != NULL){
+	//printf("%s\n",str);
+	bool result = interpret(str);
+	if(result) printf("Success\n");
+	printf("Please input instruction: ");
+	p = fgets(str,20,stdin);
+	}
+	
+	printRegistry();
 	return 0;
 }
